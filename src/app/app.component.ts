6d4b92e3240fc4b45a118299ecc65c63dd396af4
Jaume Curto Castellví <jaumecurtojob@gmail.com>
2022-12-10 18:22:25 +0100

    C

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'C5_T35_A2';
+
+  arrayClientes: any[] = [];
+
+  cliente: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.cliente = this.fb.group({
+      nombre: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.email),
+      mensaje: new FormControl('', Validators.required),
+      respuesta: new FormControl('', Validators.pattern('5'))
+    });
+  }
+
+  addCliente(): void{
+    this.arrayClientes.push(this.cliente.value);
+  }
 }
